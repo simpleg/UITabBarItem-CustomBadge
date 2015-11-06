60a26a4ad779c4a219e1311153d7357931eace35
Jerome Heymonet <jerome.heymonet@alcatel-lucent.com>
2015-11-06 19:55:56 +0100

    Try to display always the custom badge

--- UITabBarItem+CustomBadge.h
+++ UITabBarItem+CustomBadge.h
@@ -9,11 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface UITabBarItem (CustomBadge)
-
-
-
 -(void) setMyAppCustomBadgeValue: (NSString *) value;
-
 -(void) setCustomBadgeValue: (NSString *) value withFont: (UIFont *) font andFontColor: (UIColor *) color andBackgroundColor: (UIColor *) backColor;
-
 @end
--- UITabBarItem+CustomBadge.m
+++ UITabBarItem+CustomBadge.m
@@ -12,13 +12,9 @@
 #define CUSTOM_BADGE_TAG 99
 #define OFFSET 0.6f
 
-
 @implementation UITabBarItem (CustomBadge)
 
-
--(void) setMyAppCustomBadgeValue: (NSString *) value
-{
-    
+-(void) setMyAppCustomBadgeValue: (NSString *) value {
     UIFont *myAppFont = [UIFont systemFontOfSize:13.0];
     UIColor *myAppFontColor = [UIColor whiteColor];
     UIColor *myAppBackColor = [UIColor lightGrayColor];
@@ -26,31 +22,20 @@
     [self setCustomBadgeValue:value withFont:myAppFont andFontColor:myAppFontColor andBackgroundColor:myAppBackColor];
 }
 
-
-
--(void) setCustomBadgeValue: (NSString *) value withFont: (UIFont *) font andFontColor: (UIColor *) color andBackgroundColor: (UIColor *) backColor
-{
+-(void) setCustomBadgeValue: (NSString *) value withFont: (UIFont *) font andFontColor: (UIColor *) color andBackgroundColor: (UIColor *) backColor {
     UIView *v = [self valueForKey:@"view"];
-    
     [self setBadgeValue:value];
     
-    
-
-    for(UIView *sv in v.subviews)
-    {
-        
+    for(UIView *sv in v.subviews) {
         NSString *str = NSStringFromClass([sv class]);
-        
-        if([str isEqualToString:@"_UIBadgeView"])
-        {
-            for(UIView *ssv in sv.subviews)
-            {
+        if([str isEqualToString:@"_UIBadgeView"]) {
+            for(UIView *ssv in sv.subviews) {
                 // REMOVE PREVIOUS IF EXIST
-                if(ssv.tag == CUSTOM_BADGE_TAG) { [ssv removeFromSuperview]; }
+                if(ssv.tag == CUSTOM_BADGE_TAG)
+                    [ssv removeFromSuperview];
             }
             
             UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, sv.frame.size.width, sv.frame.size.height)];
-            
             
             [l setFont:font];
             [l setText:value];
@@ -67,17 +52,17 @@
             sv.layer.borderWidth = 1;
             sv.layer.borderColor = [[UIColor whiteColor] CGColor];
             sv.layer.cornerRadius = sv.frame.size.height/2;
-            sv.layer.masksToBounds = YES;
-            
-            
+            sv.layer.masksToBounds = NO;
+
             [sv addSubview:l];
+            [sv bringSubviewToFront:l];
+            
+            sv.layer.transform = CATransform3DIdentity;
+            sv.layer.transform = CATransform3DMakeTranslation(-5.0, 15.0, 20.0);
             
             l.tag = CUSTOM_BADGE_TAG;
+            [sv setNeedsLayout];
         }
     }
 }
-
-
-
-
 @end
